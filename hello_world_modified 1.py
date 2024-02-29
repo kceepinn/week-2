4164d0501de957228cdbd976ad63a4551433e6fa
kceepinn <160785804+kceepinn@users.noreply.github.com>
2024-02-29 14:18:51 +1300

    Add files via upload

---
+++ hello_world_modified 1.py
@@ -0,0 +1,57 @@
+print("*** This program shows the use of a variable. ***")
+
+# mytext is holding the value "Hello World!"
+
+my_text = "Hello World!" 
+print(my_text)
+
+# my_text is holding a new value
+
+my_text = "Dear Reader, how are you?" 
+print(my_text)
+
+
+number1=2
+number2=3
+print(number1+number2)
+number1=2.0
+print(number1+number2)
+
+
+number1=2
+print (type(number1))
+number1=2.0
+print (type(number1))
+
+
+for_variable=25
+print (for_variable) 
+
+
+print ( abs(23.7))
+print ( abs(-5.7))
+print( abs(3-7.5))
+
+
+student_1="John"
+print(student_1)
+student2="Sam"
+print(student2)
+student3="Kate"
+print(student3)
+
+
+student_name="Jack"
+print (student_name)
+
+
+x,y,z = 10,25,-303.5
+print(x)
+print(y)
+print(z)
+
+
+x=y=z=100
+print(x)
+print(y)
+print(z) 
---
+++ sum 1.py
@@ -0,0 +1,40 @@
+# comments, variables, operators
+
+multiline =  """
+How are you?                                
+
+Are you ok?     
+"""
+
+print(multiline)
+
+# Testing whether 2 is greater than 1 print(2>1)
+
+print(2>1)
+print(2*3)
+
+#strings
+
+print("Hello World!")
+
+my_text = "Hello World!" 
+
+print(my_text)
+
+print("This is Sam's book.")
+print('This is Sam\'s book.')
+
+print(1)  
+print(5.7)   
+print(-6.789)   
+
+# using variables
+my_int=125 
+print(my_int)
+my_float=25.763 
+print(my_float)
+
+# differents between numbers and strings
+
+print("1"+ "2")  
+print(1+2)
